9fc540c0c38deac74783c29e8a0ca467a88de80e
Nishanth <nisanthrtts@gmail.com>
2023-10-07 18:52:13 +0530

    final fixes

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,13 +1,13 @@
 const HeroSection = () => {
   return (
-    <div className='flex flex-col sm:justify-center pt-[86px] sm:mt-0 items-start h-[92vh] w-auto  px-[20px] sm:pl-[68px] hero-bg-sm sm:hero-bg'>
-      <p className='flex flex-col text-[40px] sm:text-[64px] font-[500] leading-[43px] sm:leading-[62px] freight-font text-[#23262F] sm:text-[#fff] mb-[24px] w-[315px] sm:w-[578.829px]'>
-        <span className=''>Safest investment on earth with </span>
+    <div className='flex flex-col sm:justify-center pt-[76px] md:pt-[60px] sm:mt-0 items-start h-auto w-auto  px-[20px] sm:pl-[68px] hero-bg-sm sm:hero-bg'>
+      <p className='flex sm:block flex-col text-[40px] sm:text-[64px] font-[500] leading-[43px] sm:leading-[62px] freight-font text-[#23262F] sm:text-[#fff] mb-[24px] w-[315px] sm:w-[578.829px]'>
+        Safest investment on earth with
         <span className='text-[#FF4F17] sm:text-[#fff]'>
           guaranteed returns of 18% p.a.
         </span>
       </p>
-      <button className='bg-[#292C6D] text-white text-[14px] font-[700] leading-[18.2px] py-[14px] px-[20.40px] rounded-[120px] uppercase'>
+      <button className='bg-[#292C6D] text-white text-[14px] font-[700] leading-[18.2px] py-[14px] px-[24.40px] rounded-[120px] uppercase'>
         Invest Now
       </button>
     </div>
